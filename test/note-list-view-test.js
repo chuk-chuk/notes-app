a1c2efe149b4e00784f69eb286c470d0b4ed1a32
chuk-chuk <yulia.v.litvin@gmail.com>
2017-08-16 17:47:56 +0100

    add string limitation to 20 chars

--- test/note-list-view-test.js
+++ test/note-list-view-test.js
@@ -1,8 +1,8 @@
 function noteListView() {
   var noteList = new NoteList(Note);
-  noteList.addNote("Hello");
+  noteList.addNote("Something new today");
   var listView = new ListView(noteList);
-  assert.isTrue(listView.show() === "<ul><li><div>Hello</div></li></ul>");
+  assert.isTrue(listView.show() === "<ul><li><div>Something </div></li></ul>");
 }
 
 noteListView();
