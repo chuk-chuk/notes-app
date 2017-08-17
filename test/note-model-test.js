8bb99aa632e51179c71b1d76c0861074456c9311
Alexander-Blair <zanderb@hotmail.co.uk>
2017-08-17 12:21:58 +0100

    Note models are created with unique ID. Moved tests into specrunner file

--- test/note-model-test.js
+++ test/note-model-test.js
@@ -1,6 +1,19 @@
 describe("noteModel", function(){
+  var note_0 = new Note("My favourite language is JavaScript");
+  var note_1 = new Note("I like ham sandwiches");
+  var note_2 = new Note("This is the best notes app ever");
+
   it("created with some text", function(){
-    var note = new Note("My favourite language is JavaScript");
-    assert.isTrue(note.getText() === "My favourite language is JavaScript");
+    assert.isTrue(note_0.getText() === "My favourite language is JavaScript");
+  });
+
+  it("is created with a unique ID (FIRST NOTE)", function() {
+    assert.isTrue(note_0.id === 0);
+  });
+  it("is created with a unique ID (SECOND NOTE)", function() {
+    assert.isTrue(note_1.id === 1);
+  });
+  it("is created with a unique ID (THIRD NOTE)", function() {
+    assert.isTrue(note_2.id === 2);
   });
 });
