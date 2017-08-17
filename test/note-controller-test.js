1914ad5285e341fc63785f90c70af01da5af8caf
chuk-chuk <yulia.v.litvin@gmail.com>
2017-08-17 15:57:12 +0100

    Load the content for the single note page tests

--- test/note-controller-test.js
+++ test/note-controller-test.js
@@ -1,6 +1,11 @@
 describe("note controller", function(){
 
   var elementMock = {};
+
+  var noteModelMock = {
+    id: 0
+  };
+
   var noteListMock = {
     addNote: function(){},
     getList: function(){
@@ -8,17 +13,50 @@ describe("note controller", function(){
     }
   };
 
+
+  function WindowMock(){ this.listenerCallCount = 0;}
+
+  WindowMock.prototype.addEventListener = function(toListen, fn){
+    this.listenerCallCount++;
+    this.listeningFor = toListen;
+  };
+
+  WindowMock.prototype.location = {
+    hash: "notes/0"
+  };
+
   var HTMLString = "<ul><li><div>Something new today</div></li></ul>";
-  
+  var singleHTMLString = "<div>Something new today</div>";
+
   function NoteListViewMock(){}
   NoteListViewMock.prototype.getHTML = function(){
     return HTMLString;
   };
 
-  var noteController = new NoteController(elementMock, noteListMock, NoteListViewMock);
+  function SingleNoteMock(){}
+  SingleNoteMock.prototype.returnHTML = function(){
+    return singleHTMLString;
+  };
+  var windowMock = new WindowMock();
+  var noteController = new NoteController(windowMock, elementMock, noteListMock, NoteListViewMock, SingleNoteMock);
 
   it("updates inner HTML property of the element", function(){
     noteController.updateDisplay();
     assert.isTrue(elementMock.innerHTML === HTMLString);
+  });
+
+  noteController.setupHashListener();
+
+  it("calls add event listener function", function(){
+    assert.isTrue(windowMock.listenerCallCount === 1);
+  });
+
+  it("sets up window to listen for hashchange", function(){
+    assert.isTrue(windowMock.listeningFor === "hashchange");
+  });
+
+  it("updates HTML property with a single note", function(){
+    noteController.updateURL();
+    assert.isTrue(elementMock.innerHTML === singleHTMLString);
   });
 });
--- test/note-list-view-test.js
+++ test/note-list-view-test.js
@@ -7,12 +7,23 @@ describe("noteListView", function(){
   };
 
   var noteModelMock = {
+    id: 0,
     getText: function(){
       return "Something new today";
     }
   };
 
+  var secondNoteModelMock = {
+    id: 1,
+    getText: function(){
+      return "Something new today again";
+    }
+  };
+
   var listView = new ListView(noteListMock);
+  var firstNote = "<li><a href='#notes/0'>Something new today</a></li>";
+  var newFirstNote = "<li><a href='#notes/0'>I like JavaScript, b</a></li>";
+  var secondNote = "<li><a href='#notes/1'>Something new today </a></li>";
 
   it("it created with a notelist", function(){
     assert.isTrue(listView.list === noteListMock);
@@ -20,14 +31,22 @@ describe("noteListView", function(){
 
   it("converts notes into HTML string", function(){
     noteListMock.addNote("Something new today");
-    assert.isTrue(listView.getHTML() === "<ul><li><div>Something new today</div></li></ul>");
+    assert.isTrue(listView.getHTML() === "<ul>" + firstNote + "</ul>");
   });
 
   it("returns a max of 20chars of HTML string", function(){
     noteModelMock.getText = function(){
       return "I like JavaScript, but it is not as nice as Ruby";
     };
-    assert.isTrue(listView.getHTML() === "<ul><li><div>I like JavaScript, b</div></li></ul>");
+    assert.isTrue(listView.getHTML() === "<ul>"+ newFirstNote + "</ul>");
   });
 
+  it("adds URL link to all notes", function(){
+    noteListMock.addNote("Something new today");
+    noteListMock.getList = function(){
+      return [noteModelMock, secondNoteModelMock];
+    };
+
+    assert.isTrue(listView.getHTML() === "<ul>" + newFirstNote + secondNote + "</ul>");
+  });
 });
